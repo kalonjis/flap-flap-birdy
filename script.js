2982e88d8eb82e3f459a35d1d65b9bb36c6896e2
kalonjis <kalonjistephane@gmail.com>
2021-10-18 13:55:14 +0200

    animation of the background -1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ img.src = './media/flappy-bird-set.png';
 //general setting
 let gamePlaying = false
 const gravity = 0.5;
-const speed = 6.2;
+const speed = 4.2;
 const size = [51, 36]
 const jump = -11.5;
 const cTenth = (canvas.width / 10);
@@ -22,6 +22,11 @@ let index = 0,
 const render = () =>{
     index++;
 
+
+    //background
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed /2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height )
+
+    // bird
     ctx.drawImage(img , 432, Math.floor((index % 9) / 3) * size[1] , ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
     flyHeight = (canvas.height / 2) - (size[1] / 2);
 
