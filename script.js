28e78ede25ceec953cd6e440483edef54047f25d
kalonjis <kalonjistephane@gmail.com>
2021-10-18 12:48:19 +0200

    general settings before any animation

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = './media/flappy-bird-set.png';
 
+
+//general setting
+let gamePlaying = false
+const gravity = 0.5;
+const speed = 6.2;
+const size = [51, 36]
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0;
+    bestScore = 0,
+    currentScore = 0,
+    pipes = []
+    flight,
+    flyHeight;
+
+const render = () =>{
+    index++;
+
+
+    window.requestAnimationFrame(render)
+}
